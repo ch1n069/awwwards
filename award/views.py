e09b31a6419f9fff7574fce11ad226b8b8f0dd07
bruno <brunolalpachino@gmail.com>
2022-06-12 23:52:41 +0300

    used class based views for home

--- award/views.py
+++ award/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
-
+from django.views.generic.base import TemplateView
 # Create your views here.
 
+class Home(TemplateView):
+    template_name = 'award/home.html'
 
-
-def Home(request):
-    return render(request, 'award/home.html')
+# def Home(request):
+#     return render(request, 'award/home.html')
