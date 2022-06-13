c1234c694da73d3880002564d8b0676d61ad6638
bruno <brunolalpachino@gmail.com>
2022-06-13 15:50:43 +0300

    created model for the projects

--- award/models.py
+++ award/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField() 
+    url = models.URLField(blank=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
