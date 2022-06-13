887d302b493b7afbdd8bf9d626c279dabcf99c42
bruno <brunolalpachino@gmail.com>
2022-06-13 14:31:25 +0300

    corrected the urls.py

--- award/urls.py
+++ award/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path 
 from award import views
 
 urlpatterns = [
-    path('', views.Home, name='home'),
-    # path('' , views.Home.as_view(), name="home"),
+    # path('', views.Home, name='home'),
+    path('' , views.Home.as_view(), name="home"),
+    path('signup/' , views.signup, name="register"),
+
 ]
